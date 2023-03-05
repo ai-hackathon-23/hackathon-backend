b052845b1ba35f1eb5bdf1bb1f589bb536945202
kyoya <kyoyaprog@gmail.com>
2023-03-06 06:24:19 +0900

    インフラ構築

---
+++ mysql/init.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE IF NOT EXISTS mydb;
+USE mydb;
+
+CREATE TABLE States (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    disease VARCHAR(255) NOT NULL,
+    treatments JSON NOT NULL,
+    medicines JSON NOT NULL,
+    treatment_policy VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE CarePlans (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    author VARCHAR(255) NOT NULL,
+    facility_name VARCHAR(255) NOT NULL,
+    result_analyze VARCHAR(255),
+    care_committee_opinion VARCHAR(255),
+    specified_service VARCHAR(255),
+    care_policy VARCHAR(255)
+);
+
+CREATE TABLE Clients (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    disease VARCHAR(255) NOT NULL,
+    family_living_together JSON,
+    state_id INT,
+    care_plan_id INT,
+    FOREIGN KEY (state_id) REFERENCES States(id),
+    FOREIGN KEY (care_plan_id) REFERENCES CarePlans(id)
+);
+
